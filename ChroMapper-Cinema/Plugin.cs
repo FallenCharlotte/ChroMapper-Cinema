a539b66b2286398e60ee20ba6cc57729d15a3f84
Moizac <moistsac@gmail.com>
2022-11-26 10:51:20 -0800

    Project stub

---
+++ ChroMapper-Cinema/Plugin.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using ChroMapper_Cinema.UserInterface;
+
+namespace ChroMapper_Cinema {
+
+[Plugin("Cinema")]
+public class Plugin {
+	public static UI ui;
+	
+	[Init]
+	private void Init() {
+		Debug.Log("Cinema Plugin has loaded!");
+		SceneManager.sceneLoaded += SceneLoaded;
+		ui = new UI();
+	}
+	
+	private void SceneLoaded(Scene scene, LoadSceneMode mode) {
+		if (scene.buildIndex == 3) {
+			// Map Edit
+			var mapEditorUI = Object.FindObjectOfType<MapEditorUI>();
+			ui.AddWindow(mapEditorUI);
+		}
+	}
+	
+	[Exit]
+	private void Exit() {
+		
+	}
+}
+
+}
---
+++ ChroMapper-Cinema/UserInterface/UI.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using SimpleJSON;
+
+namespace ChroMapper_Cinema.UserInterface {
+
+public class UI {
+	//public ExtensionButton main_button;
+	public GameObject screen;
+	
+	public UI() {
+		/*
+		main_button = ExtensionButtons.AddButton(
+			LoadSprite("ChroMapper_Cinema.Resources.Icon.png"),
+			"Cinema",
+			ToggleWindow);
+			* */
+	}
+	
+	public void AddWindow(MapEditorUI mapEditorUI) {
+		
+	}
+}
+
+}
